282d2a0223884a0e4ad4543ade859c27aef0afb9
heather-irene <cunningham.heatherirene@gmail.com>
2025-03-06 12:06:58 -0500

    main: python main module

---
+++ main.py
@@ -0,0 +1,9 @@
+## Play2Learn3
+## main module to serve as the entry point for Python Terminal v. Play2Learn3 project
+
+print("Welcome to Play2Learn3!\nA Python text-based console/terminal game program.")
+print("Play2Learn3 contains 2 games:")
+print("-" * 65)
+print("* Anagram Hunt -- How many anagrams can you find in 60 seconds?")
+print("* Math Facts -- How many arithmetic problems can you solve in 30 seconds?")
+
