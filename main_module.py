dbec625883919617e6a835d48331c9eadcd43f1e
heather-irene <cunningham.heatherirene@gmail.com>
2025-04-03 12:53:50 -0400

    parent-classes: Added a quit method to the main_module

--- main_module.py
+++ main_module.py
@@ -2,7 +2,7 @@ from player import Player
 from anagram_game.anagram_hunt import AnagramHunt
 from math_game.math_facts import MathFacts
 from helpers.margin_separator_module import get_margin_separator
-from helpers.prompt_player import prompt_player_for_user_info, prompt_player_for_new_user_info
+from helpers.prompt_player import prompt_player_for_user_info, prompt_player_for_new_user_info, GOODBYE_MSG
 
 
 ## Play2Learn3
@@ -21,10 +21,15 @@ def greet_user():
     print("Play2Learn3 contains 2 games:")
     print(_MARGIN_STR)
     print("1.)  Anagram Hunt -- How many anagrams can you find in 60 seconds?")
-    print("2.)  Math Facts -- How many arithmetic problems can you solve in 30 seconds?\n")
+    print("2.)  Math Facts -- How many arithmetic problems can you solve in 30 seconds?")
+    print("* Type 'zzz' to quit.\n")
     ## Former college Prof. convention of explicitly returning, even if empty, to mark the end of a method.
     return
 
+
+def quit():
+    print(GOODBYE_MSG)
+    return 
 
 
 ## Allows the user to select a game to play.
@@ -34,7 +39,12 @@ def select_game():
     err_msg = f"{_MARGIN_STR}\nPlease, press 1 or 2 to select a game.\n{_MARGIN_STR}"
     while(game_to_play != 1 and game_to_play != 2):
         try:
-            game_to_play = int(input("Which game would you like to play? (1-Anagram Hunt / 2-Math Facts) "))
+            game_to_play = input(
+                "Which game would you like to play? (1-Anagram Hunt / 2-Math Facts) ['zzz' to quit] "
+            )
+            if(game_to_play == "zzz"):
+                return "" ## Return a falsy value to quit
+            game_to_play = int(game_to_play)
         except ValueError:
             print(err_msg)
         else:
@@ -73,7 +83,10 @@ def main():
     """ A friendly, text-based 'homepage'. """
     greet_user()
     game_to_play = select_game()
-    launch_game(game_to_play)
+    if(game_to_play):
+        launch_game(game_to_play)
+    else: ## User quit
+        quit()
     return    
 ## END module
 
